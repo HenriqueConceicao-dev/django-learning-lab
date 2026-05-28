ee029f529bb80cb906b90bb19e8bf4d768857814
Henrique Araujo Conceicao <henrique.conceicao@caoa.com.br>
2026-05-28 18:04:42 -0300

    feat(django-basics): configure templates and create initial Cliente model

--- backend/01-django-basico/projeto_base/usuarios/models.py
+++ backend/01-django-basico/projeto_base/usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# orm permite trabalahr com classe e metodos do pthon
+
+class Cliente(models.Model):
+    QUEIXA_CHOICES = (('D','Depressao'), ('A', 'Ansiedade'))
+
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    queixa = models.CharField(max_length=1 , choices=QUEIXA_CHOICES)
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
