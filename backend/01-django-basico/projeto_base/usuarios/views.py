c6e1b96648e16851d06807135c3f2b557955c30b
Henrique Araujo Conceicao <henrique.conceicao@caoa.com.br>
2026-05-29 17:37:16 -0300

    feat(clientes): implement customer model and form submission flow

--- backend/01-django-basico/projeto_base/usuarios/views.py
+++ backend/01-django-basico/projeto_base/usuarios/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Cliente
 
 def cadastro(request):
     return HttpResponse('login usuario')
 
 def cliente(request):
-    return render(request, 'cliente.html')
+    if request.method == 'GET':
+        return render(request, 'cliente.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        queixa = request.POST.get('queixa')
+
+        if len(nome) <= 2 :
+            return redirect('cliente')
+        
+        cliente = Cliente(nome=nome, email=email, queixa=queixa)
+
+        cliente.save()
+
+        return redirect('cliente')
+
+
+    
